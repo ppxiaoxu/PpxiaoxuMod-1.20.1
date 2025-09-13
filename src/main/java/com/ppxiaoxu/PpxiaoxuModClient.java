f377eb4565dc9cadf102701341b9e24e3409dc6b
ppxiaoxu <hello@ppxiaoxu.cn>
2025-09-13 16:23:06 +0800

    Initial commit

---
+++ src/main/java/com/ppxiaoxu/PpxiaoxuModClient.java
@@ -0,0 +1,10 @@
+package com.ppxiaoxu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PpxiaoxuModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/ppxiaoxu/PpxiaoxumodDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ppxiaoxu;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PpxiaoxumodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
